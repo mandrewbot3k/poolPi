603827de8c979ea924341d46c5ebe8ebb4284f81
Andrew Malizia <andrew.malizia@gmail.com>
2018-06-25 23:17:03 +0000

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,21 @@ var weather = require('weather-js');
 var configFile = require('../data/config.json');
 
 var location, weatherUnit, weatherTemp, weatherCode, weatherDesc;
-weather.find({search: configFile.location, degreeType: 'F'}, function(err, result) {
- if(err) console.log(err);
-        //weatherRes = JSON.parse(result);
-        location = result[0]['location']['name'];
-        weatherUnit = result[0]['location']['degreetype'];
-        weatherTemp = result[0]['current']['temperature'];
-        weatherCode = result[0]['current']['skycode'];
-        weatherDesc = result[0]['current']['skytext'];
-console.log("It's currently "+ weatherTemp + " " + weatherUnit +" in " + location);
-console.log("#dev from home page");
-});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+  weather.find({search: configFile.location, degreeType: 'F'}, function(err, result) {
+   if(err) console.log(err);
+          //weatherRes = JSON.parse(result);
+          location = result[0]['location']['name'];
+          weatherUnit = result[0]['location']['degreetype'];
+          weatherTemp = result[0]['current']['temperature'];
+          weatherCode = result[0]['current']['skycode'];
+          weatherDesc = result[0]['current']['skytext'];
+  console.log("It's currently "+ weatherTemp + " " + weatherUnit +" in " + location);
+  console.log("#dev from home page");
+  });
   res.render('index', {
       title: 'poolPi',
       heading: 'Home',
