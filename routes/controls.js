841d44ad886f23cd193c53e640dee3ebaa6b813f
Andrew Malizia <andrew.malizia@gmail.com>
2018-06-29 07:10:15 +0000

    06.29.18

--- routes/controls.js
+++ routes/controls.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var devs = require('../data/devices.json')
+
+var deviceTypes, mineDevices;
+
+var types = function(req, res, next){
+  // list all device types
+  devs.devices.types.forEach(function(deviceTypes){
+
+    console.log(deviceTypes);
+  });
+
+  // list all devices
+  devs.devices.mydevices.forEach(function(mineDevices){
+    //if(err) console.log(err);
+    console.log(mineDevices);
+  })
+  next();
+};
+
+router.use(types);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('controls', {
       title: 'poolPi',
       heading: 'Controls',
-      pageID: 'controls'
+      pageID: 'controls',
+      myDevices: mineDevices,
+      deviceTypes: deviceTypes
 });
 
 });
--- routes/scheduler.js
+++ routes/scheduler.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var sch = require('../data/schedule.json');
+var schedule = require('node-schedule');
 
 app = express();
+console.log('hello from the schedule');
 
 /* GET home page. */
 router.use(function setTimers(req, res, next){
@@ -15,21 +17,28 @@ router.use(function setTimers(req, res, next){
 //RULES
     // START TIME
       //rules
-      var startrule = []; //new schedule.RecurrenceRule();
+      var startrule = new schedule.RecurrenceRule();
       startrule.hour = st.h;
       startrule.minute = st.m;
       startrule.daysofweek = daysofweek;
       console.log("[" + item.ID + "]" +item.description+":  Start Time: " + startrule.hour + ":" + startrule.minute);
       // Add Start Action here
+      var j = schedule.scheduleJob(startrule, function(){
+  console.log('start Rule!');
+});
 
     //End Time
-      var endrule = []; //new schedule.RecurrenceRule();
+      var endrule = new schedule.RecurrenceRule();
       endrule.hour = et.h;
       endrule.minute = et.m;
       endrule.daysofweek = daysofweek;
       console.log("End Time: " + endrule.hour +":"+ endrule.minute);
       // Add end action here:
 
+
+      var k = schedule.scheduleJob(endrule, function(){
+        console.log('End Rule!');
+      });
     });
 next();
 });
